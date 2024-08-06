5faa659de7a6a1d3bed4e513a185a86d865e9508
xiaojingge <2427259171@qq.com>
2024-08-06 19:17:56 +0800

    feat: ✨ 情感分析初始化

---
+++ service/task_sentiment_classification/sentiment_classification_service.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@Time    : 2024-07-31 13:49
+@Author  : lijing
+@File    : sentiment_classification_service.py
+@Description: 情感分类业务
+---------------------------------------
+"""
