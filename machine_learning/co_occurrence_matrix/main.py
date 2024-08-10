20aee0122e5be453796056427323932b405f9337
xiaojingge <2427259171@qq.com>
2024-08-10 14:27:20 +0800

    feat: ✨ 共现矩阵学习

---
+++ machine_learning/co_occurrence_matrix/main.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+'''
+---------------------------------------
+@Time    : 2024-08-10 14:01
+@Author  : lijing
+@File    : main.py
+@Description: 
+---------------------------------------
+'''
+import sys
+from os import path
+# pip3 install python-docx
+from docx import Document
+import chnSegment
+import plot_wordcloud
+import co_occurrence
+
+"""
+pip3 install jieba
+pip3 install wordcloud
+pip3 install python-docx
+pip3 install matplotlib
+pip3 install numpy
+pip3 install seaborn
+pip3 install networkx
+"""
+
+if __name__ == '__main__':
+
+    # 读取文件 默认读取demo.txt 如有附加参数d 则读取demo.docx
+    d = path.dirname(__file__)
+    file_path = 'doc/demo.txt'  # 要读取的文件名称
+    if len(sys.argv) > 1:
+        first_arg = sys.argv[1]
+        # print(f"The first argument is: {first_arg}")
+        if first_arg == 'd':
+            file_path = 'doc/demo.docx'
+
+            # 获取文件扩展名
+    file_extension = path.splitext(file_path)
+    content = ''
+    if file_extension[1].lower() == '.txt':
+        # 读取 .txt 文件
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+    elif file_extension[1].lower() == '.docx':
+        # 使用 python-docx 读取 .docx
+        doc = Document(file_path)
+        content = '\n'.join([para.text for para in doc.paragraphs])
+    else:
+        print("未知文件类型")
+
+        # 若是中文文本，则先进行分词操作
+    text = chnSegment.word_segment(content)
+
+    # 生成词云
+    plot_wordcloud.generate_wordcloud(text)
+
+    # 共现矩阵
+    co_occurrence.generate_pic(text)
---
+++ machine_learning/co_occurrence_matrix/plot_wordcloud.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+'''
+---------------------------------------
+@Time    : 2024-08-10 13:57
+@Author  : lijing
+@File    : plot_wordcloud.py
+@Description: 
+---------------------------------------
+'''
+from os import path
+from PIL import Image
+from wordcloud import WordCloud
+import numpy as np
+
+stopwords_path = 'userdict/stopword.txt'
+stopwords = set(line.strip() for line in open(stopwords_path, encoding='utf-8'))
+
+
+def generate_wordcloud(text):
+    '''
+    输入文本生成词云,如果是中文文本需要先进行分词处理
+    '''
+    # 设置显示方式
+    # print(text)
+    d = path.dirname(__file__)
+    alice_mask = np.array(Image.open(path.join(d, "Images//alice_mask1.jpg")))
+    font_path = path.join(d, "font//msyh.ttf")
+    wc = WordCloud(
+        background_color="white",  # 设置背景颜色  "silver"
+        # color_func=lambda *args, **kwargs: "black" , # 设置字体颜色
+        stopwords=stopwords,  # 设置停用词 无法为空，因此再设置一次停用词
+        max_words=100,  # 词云显示的最大词数
+        collocations=False,  # 可防止出现重复
+        # width=600,  # 宽度
+        # height=400,   # 高度
+        max_font_size=150,  # 最大字的最大字体大小
+        min_font_size=10,  # 最小字体大小
+        mask=alice_mask,  # 设置背景图片
+        font_path=font_path,  # 兼容中文字体，不然中文会显示乱码
+        prefer_horizontal=1.01,  # 文字横向和竖向之比，当大于1时文字全部横着。默认为0.9
+    )
+    # 生成词云
+    wc.generate(text)
+
+    # 生成的词云图像保存到本地
+    print("词云图:images/wordCloud.png")
+    wc.to_file(path.join(d, "Images//wordCloud.png"))
